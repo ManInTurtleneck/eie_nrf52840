667a59b8d6977df6713b02ebb84b42ad911e8650
Django Schmidt <86383936+ManInTurtleneck@users.noreply.github.com>
2025-10-02 20:16:05 -0600

    Heartbeat

--- app/src/main.c
+++ app/src/main.c
@@ -5,9 +5,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-#define LED0_NODE DT_ATIAS(led0)
+#define LED0_NODE DT_ALIAS(led0)
 
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gios);
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void) {
 
@@ -23,7 +23,9 @@ int main(void) {
   }
 
   while (1) {
+    gpio_pin_toggle_dt(&led0);
 
+    k_msleep(1000);
   }
 
   return 0;
